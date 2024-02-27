2baaa1fc7cbc05adadef1971dbdd989cc13e39bc
comppoo <91119897+comppoo@users.noreply.github.com>
2024-02-27 15:16:57 +0800

    Update cy.js

--- plugin/cy.js
+++ plugin/cy.js
@@ -1,128 +1,59 @@
+const c1000 = {};
+const c1011 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+const url = $request.url;
+const adUrl = /(activity\?app_name|operation\/banners)/;
+const vipUrl = /https:\/\/biz\.(cyapi\.cn|caiyunapp\.com)\/p\/v\d\/vip_info/;
+const userUrl = /https:\/\/biz\.(cyapi\.cn|caiyunapp\.com)\/v\d\/user\?app_name/;
+const infoUrl = /https:\/\/biz\.(cyapi\.cn|caiyunapp\.com)\/p\/v\d\/user_info/;
+const aiUrl = /ai\/weather\/quotas/;
+const qyUrl = /entries/;
+const peUrl = /privileges/;
+const topUrl = /operation\/homefeatures/;
 
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
-
-const p1 = '/v1/user_info';
-const p2 = '/v2/user';
-
-//个人页面
-if (url.indexOf(p1) != -1) {
-    obj.name = "https://t.me/GieGie777";
-    obj.avatar = "https://zdimg.lifeweek.com.cn/app/20230410/16810960185662892.jpg";
-    body = JSON.stringify(obj);
-}
-
-//_vip
-if (url.indexOf(p2) != -1) {
-    obj = {
-        "server_time": 1708353391,
-        "result": {
-            "xy_vip_expire": 0,
-            "vip_expired_at": 0.0,
-            "ranking_above": 97.0,
-            "vip_take_effect": 1,
-            "auto_renewal_type": "",
-            "third_party_id": null,
-            "is_kol": false,
-            "svip_expired_at": 32493834549.0,
-            "is_login": false,
-            "is_xy_vip": false,
-            "platform_name": "weixin",
-            "xy_svip_expire": 0,
-            "is_visitor": false,
-            "third_party_ids": ["6406c68989e1a20018905c8d"],
-            "score": 2000,
-            "is_xy_auto_renewal": false,
-            "is_primary": true,
-            "phone_num": "17083544841",
-            "free_trial": 10,
-            "last_acted_at": 1699229543.0828011036,
-            "vip_type": "s",
-            "is_phone_verified": true,
-            "wt": {
-                "ranking_above": 97.0,
-                "created_at": 1599863895.1142549515,
-                "last_acted_at": 1699229543.0828011036,
-                "vip": {
-                    "svip_apple_expired_at": 1692783499,
-                    "enabled": true,
-                    "is_auto_renewal": true,
-                    "auto_renewal_type": "",
-                    "svip_auto_renewal_type": "month",
-                    "svip_expired_at": 1851501031.0,
-                    "expired_at": 0.0
-                },
-                "svip_given": 1825,
-                "free_trial": 10,
-                "is_login": false
-            },
-            "device_id": "0D5E89DC-78F7-40A8-9348-E5BD077236C8-1699229500",
-            "name": "https://t.me/GieGie777",
-            "svip_take_effect": 1,
-            "bound_status": {
-                "qq": {
-                    "username": "",
-                    "is_bound": false,
-                    "id": ""
-                },
-                "weibo": {
-                    "username": "",
-                    "is_bound": false,
-                    "id": ""
-                },
-                "google": {
-                    "username": "",
-                    "is_bound": false,
-                    "id": ""
-                },
-                "apple": {
-                    "username": "",
-                    "is_bound": true,
-                    "id": "6406c68989e1a20018905c8d"
-                },
-                "weixin": {
-                    "username": "https://t.me/GieGie777",
-                    "is_bound": true,
-                    "id": "5f5bfc57d2c6890014e26bb8"
-                },
-                "caiyun": {
-                    "username": "",
-                    "is_bound": false,
-                    "id": ""
-                },
-                "twitter": {
-                    "username": "",
-                    "is_bound": false,
-                    "id": ""
-                },
-                "facebook": {
-                    "username": "",
-                    "is_bound": false,
-                    "id": ""
-                }
-            },
-            "created_at": 1599863895.1142361164,
-            "is_auto_renewal": true,
-            "hasBeenInvited": true,
-            "platform_id": "o3rJ_t00r0mxqS6GCVWMaVtEFLUk",
-            "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoiNjVkMzY2OTRmY2FjMDgwMDE0NDlkODMzIiwidXNlcl9pZCI6IjVmNWJmYzU3ZDJjNjg5MDAxNGUyNmJiOCIsInZlcnNpb24iOjIsImV4cCI6MTcxNjEyOTE3MiwidmlwX2V4cGlyZWRfYXQiOjAsImlzcyI6IndlYXRoZXIiLCJpYXQiOjE3MDgzNTMxNzIsInN2aXBfZXhwaXJlZF9hdCI6MTg1MTUwMTAzMSwicHJpbWFyeSI6dHJ1ZX0.U_LCYNGl3cbBCpjrnjmxYBAuNFSvNCe3dKIrV1MGpzo",
-            "svip_given": 1825,
-            "avatar": "https://avatar.caiyuncdn.com/avatar/39cff6272f6e133d731fc6fda973e533-1708017050091.jpg",
-            "is_vip": true,
-            "gender": null,
-            "_id": "5f5bfc57d2c6890014e26bb8"
-        },
-        "rc": 0
+if (typeof $response == "undefined") {
+  c1000.headers = $request.headers;
+  c1000.headers['device-token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoiNjViYzc1NzAwYTBkNTYwMDFiZmJkODhjIiwidXNlcl9pZCI6IjVmODNhMDY3MTk5NzZmMDAxNWEyNTRiNiIsInZlcnNpb24iOjIsImV4cCI6MTcwOTQ4MjAwMCwidmlwX2V4cGlyZWRfYXQiOjAsImlzcyI6IndlYXRoZXIiLCJpYXQiOjE3MDY4OTAwMDAsInN2aXBfZXhwaXJlZF9hdCI6MTcxNDMyMTgxMiwicHJpbWFyeSI6dHJ1ZX0.v41eOWgj4FmMMYLygupRLeE2hC8KW_HltSsdFk03oP4';
+} else {
+  switch (true) {
+    case adUrl.test(url):
+      c1011.status = "ok";
+      c1011.activities = [{"items":[{}]}];
+      c1011.data = [];
+      break;
+    case vipUrl.test(url):
+      c1011.vip = {  ...c1011.vip,
+  "expires_time" : "4092599349",  "is_auto_renewal" : true  };
+      c1011.svip =  {  ...c1011.svip,  "expires_time" : "4092599349",  "is_auto_renewal" : true  };
+      c1011.show_upcoming_renewal = false;
+      break;
+    case userUrl.test(url):
+      c1011.result = { ...c1011.result,  is_vip: true,  vip_expired_at: 4092599349,  svip_given: 1,  is_xy_vip: true,  xy_svip_expire: 4092599349,  wt: {  ...c1011.result.wt,  vip: {  ...c1011.result.wt.vip,  "expired_at" : 0,  "enabled" : true,  "svip_apple_expired_at" : 4092599349,  "is_auto_renewal" : true,  "svip_expired_at" : 4092599349    },    svip_given: 1,  },  is_phone_verified: true,  vip_take_effect: 1,  is_primary: true,  xy_vip_expire: 4092599349,  svip_expired_at: 4092599349,  svip_take_effect: 1,  vip_type: "s",  phone_num: "13145200000",  name: "222,  avatar: "https://raw.githubusercontent.comg",  bound_status: {  ...c1011.result.bound_status,  caiyun: {  ...c1011.result.bound_status.caiyun,  "username" : "222,  "is_bound" : true}}};
+      break;
+    case infoUrl.test(url):
+      c1011["reg_days"] = 99999;
+      c1011["name"] = "22";
+      c1011["avatar"] = "https://raw.githubusercontent";
+      break;
+    case aiUrl.test(url):
+      c1011.remain = 999;
+      c1011.subscription_remain = 999;
+      c1011.subscription_quota = 999;
+      c1011.addition_remain = 999;
+      c1011.subscription_quota_end_time = 4092599349;
+      c1011.free_remain = 999;
+      c1011.free_quota = 999;
+      break;
+    case qyUrl.test(url):
+      c1011["entries"] = [{  "url" : "https://t.m3",  "id" : 1,  "name" : "222",  "type" : 1,  "pos" : 2  }];
+      break;
+    case peUrl.test(url):
+      c1011["privileges"] = [{  "vip_type" : "svip",  "subscription_chat_quota" : 999  }];
+      break;
+    case topUrl.test(url):
+      c1011["data"] = [{  "badge_type" : "",  "title" : "222",  "url" : "https://",  "feature_type" : "",  "avatar" : "https://raw.githubusercontent.com"  },...c1011.data];
+      break;
     }
-    $done({
-        body:  JSON.stringify(obj)
-    });
-    body = JSON.stringify(obj);
+  c1000.body = JSON.stringify(c1011);
 }
 
-
-
-$done({
-    body
-});
+$done(c1000);
