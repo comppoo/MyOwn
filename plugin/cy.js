b19901d4d9c8ee38ee37d68e69d3f84b0cdd18d1
comppoo <91119897+comppoo@users.noreply.github.com>
2024-02-27 15:32:06 +0800

    Update cy.js

--- plugin/cy.js
+++ plugin/cy.js
@@ -1,3 +1,4 @@
+
 const c1000 = {};
 const c1011 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 const url = $request.url;
@@ -27,12 +28,10 @@ if (typeof $response == "undefined") {
       c1011.show_upcoming_renewal = false;
       break;
     case userUrl.test(url):
-      c1011.result = { ...c1011.result,  is_vip: true,  vip_expired_at: 4092599349,  svip_given: 1,  is_xy_vip: true,  xy_svip_expire: 4092599349,  wt: {  ...c1011.result.wt,  vip: {  ...c1011.result.wt.vip,  "expired_at" : 0,  "enabled" : true,  "svip_apple_expired_at" : 4092599349,  "is_auto_renewal" : true,  "svip_expired_at" : 4092599349    },    svip_given: 1,  },  is_phone_verified: true,  vip_take_effect: 1,  is_primary: true,  xy_vip_expire: 4092599349,  svip_expired_at: 4092599349,  svip_take_effect: 1,  vip_type: "s",  phone_num: "13145200000",  name: "222,  avatar: "https://raw.githubusercontent.com",  bound_status: {  ...c1011.result.bound_status,  caiyun: {  ...c1011.result.bound_status.caiyun,  "username" : "222,  "is_bound" : true}}};
+      c1011.result = { ...c1011.result,  is_vip: true,  vip_expired_at: 4092599349,  svip_given: 1,  is_xy_vip: true,  xy_svip_expire: 4092599349,  wt: {  ...c1011.result.wt,  vip: {  ...c1011.result.wt.vip,  "expired_at" : 0,  "enabled" : true,  "svip_apple_expired_at" : 4092599349,  "is_auto_renewal" : true,  "svip_expired_at" : 4092599349    },    svip_given: 1,  },  is_phone_verified: true,  vip_take_effect: 1,  is_primary: true,  xy_vip_expire: 4092599349,  svip_expired_at: 4092599349,  svip_take_effect: 1,  vip_type: "s",  };
       break;
     case infoUrl.test(url):
       c1011["reg_days"] = 99999;
-      c1011["name"] = "222";
-      c1011["avatar"] = "https://raw.githubusercontent";
       break;
     case aiUrl.test(url):
       c1011.remain = 999;
@@ -44,19 +43,16 @@ if (typeof $response == "undefined") {
       c1011.free_quota = 999;
       break;
     case qyUrl.test(url):
-      c1011["entries"] = [{  "url" : "https://t.m3",  "id" : 1,  "name" : "叮",  "type" : 1,  "pos" : 2  }];
+      c1011["entries"] = [{  "url" : "https://t.m",  "id" : 1,  "name" : "xxx",  "type" : 1,  "pos" : 2  }];
       break;
     case peUrl.test(url):
       c1011["privileges"] = [{  "vip_type" : "svip",  "subscription_chat_quota" : 999  }];
       break;
     case topUrl.test(url):
-      c1011["data"] = [{  "badge_type" : "",  "title" : "叮",  "url" : "https://1.com",  "feature_type" : "",  "avatar" : "https://raw.githubusercontent.com"  },...c1011.data];
+      c1011["data"] = [{  "badge_type" : "",  "title" : "xxx",  "url" : "https://t.m",  "feature_type" : "",  "avatar" : "https://raw.githubusercontent.com"  },...c1011.data];
       break;
     }
   c1000.body = JSON.stringify(c1011);
 }
 
 $done(c1000);
-
-
-**************************************
